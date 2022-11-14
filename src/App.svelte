583f30b948a7bd89f544574e7a48bff81068b790
Utsav Singh <utsav.raj.338@gmail.com>
2022-11-14 14:26:59 +0530

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import Header from "./lib/Header.svelte";
+  import Home from "./lib/Home.svelte";
+  import Services from "./lib/Services.svelte";
+  import Skills from "./lib/Skills.svelte";
+  import Experiences from "./lib/Experiences.svelte";
+  import Contact from "./lib/Contact.svelte";
+  import biswa from "../biswa.json";
+
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<main class="mx-2 md:mx-8">
+  <Header  />
+  <Home
+  
+    name={biswa.name}
+    tagline={biswa.tagline}
+    email={biswa.email}
+    linkedin={biswa.linkedin}
+    github={biswa.github}
+    twitter={biswa.twitter}
+    instagram={biswa.instagram}
+    medium={biswa.medium}
+  />
+  <Services services={biswa.services} />
+  <Skills technology={biswa.skills.technology} tools={biswa.skills.tools} />
+  <Experiences experiences={biswa.experience} />
+  <Contact
+    linkedin={biswa.linkedin}
+    github={biswa.github}
+    twitter={biswa.twitter}
+    instagram={biswa.instagram}
+    medium={biswa.medium}
+    email={biswa.email}
+  />
+
+  <div class="mt-24 mb-1">
+    <p>Copyright ©️ 2022, All rights reserved</p>
+    <p>
+      Made with ❤️ by {biswa.nickname}
+     
+    </p>
+  </div>
+</main>
+
+<style>
+  :root {
+    background-color: #f8f8f8;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    font-family: "Rubik", sans-serif;
+  }
+</style>
---
+++ src/lib/Experiences.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Experience from "./components/Experience.svelte";
+  export let experiences = [];
+</script>
+
+<div id="experience" class="mt-16">
+  <p
+    class="items-start text-start content-start font-bold text-4xl md:text-5xl mb-12"
+  >
+    Experience
+  </p>
+
+  {#each experiences as experience, i}
+    <Experience {experience} />
+    {#if i != experiences.length - 1}
+      <div class="my-8">
+        <div class="w-11/12 border-t border-gray-300" />
+      </div>
+    {/if}
+  {/each}
+</div>
